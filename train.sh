9f8d8ccb14b5d2a2ed4110868942ab7b04932de7
Zachacy <tpre31738@gmail.com>
2019-08-26 13:44:04 +0800

    Fisrt commit

---
+++ train.sh
@@ -0,0 +1,2 @@
+python3 monodepth_main.py --mode train --model_name pydnet_correlation --data_path /home/rvl/Dataset/stereo/KITTI/ \
+--filenames_file ./utils/filenames/kitti_train_files.txt --log_directory ~/logs --do_stereo
